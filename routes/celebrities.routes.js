0eb496051adecc1be5cc2e45a837dee91134e7e4
“Marcela <mpmontiela@gmail.com>
2022-10-31 17:10:00 +0100

    done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -4,7 +4,20 @@ const Celebrity = require('../models/Celebrity.model');
 
 // all your routes here
 
+router.get("/create", (req,res)=>{
+    res.render("celebrities/new-celebrity")
+})
 
-
+router.post("/new-celebrity", (req,res,next)=>{
+    console.log(Celebrity)
+    const {name, occupation, catchPhrase} = req.body
+    Celebrity.create({name, occupation, catchPhrase})
+        .then(newCelebrity=>{
+        console.log(newCelebrity)
+            res.redirect("/celebrities/celebrities")
+     }).catch((error)=> {
+        console.log(error)
+             res.redirect("/celebrities/new-celebrity")});
+});
 
 module.exports = router; 
