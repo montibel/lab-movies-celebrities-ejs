6bceb9392173ac4edbf2e2746d608baccb594a63
“Marcela <mpmontiela@gmail.com>
2022-10-31 16:55:38 +0100

    done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,29 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require('express').Router()
+const Celebrity = require('../models/Celebrity.model');
 
 // all your routes here
 
-module.exports = router
+//CREATE NEW CELEBRITY
+
+router.get("/create", (req,res)=>{
+    res.render("celebrities/new-celebrity")
+})
+
+router.post("/new-celebrity", (req,res,next)=>{
+    console.log(Celebrity)
+
+    const {name, occupation, catchPhrase} = req.body
+    Celebrity.create({name, occupation, catchPhrase})
+
+        .then(newCelebrity=>{
+        console.log(newCelebrity)
+            res.redirect("/celebrities/celebrities")
+     }).catch((error)=> {
+        console.log(error)
+             res.redirect("/celebrities/create")});
+});
+
+
+
+module.exports = router; 
